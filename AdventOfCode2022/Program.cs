b59503564f3d037089f880aa05d0df7684d4c548
09sstinson <09sstinson@gmail.com>
2022-12-01 09:58:29 +0000

    Solution

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2022
 {
     class Program
     {
+        const string InputFileLocation = "C:\\Users\\Home\\Documents\\Aoc2022\\Day1\\input.txt";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputStrings = File.ReadAllLines(InputFileLocation);
+
+            var calories = new List<int>();
+            var tempCalories = 0;
+            foreach (var input in inputStrings)
+            {
+                if(input == null || input.Trim() == string.Empty)
+                {
+                    calories.Add(tempCalories);
+                    tempCalories = 0;
+                }
+                else
+                {
+                    tempCalories += int.Parse(input);
+                }
+            }
+
+            Console.WriteLine(calories.Max());
         }
     }
 }
