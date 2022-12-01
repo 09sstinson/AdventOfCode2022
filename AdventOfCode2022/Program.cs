5b280704a1d79cade2dfe28d0ec08bd72dbb7bd0
09sstinson <09sstinson@gmail.com>
2022-12-01 10:14:02 +0000

    Merge

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -27,7 +27,7 @@ namespace AdventOfCode2022
                 }
             }
 
-            Console.WriteLine(calories.OrderByDescending(x => x).Take(3).Sum());
+            Console.WriteLine(calories.Max());
         }
     }
 }
