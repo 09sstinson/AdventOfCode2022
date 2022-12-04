c8791f699fbf009480c7f71ad24e28530ef421ee
09sstinson <09sstinson@gmail.com>
2022-12-04 11:24:08 +0000

    Solve Day4

---
+++ AdventOfCode2022/Solutions/Day4/Day4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace AdventOfCode2022.Solutions
+{
+    public class Day4 : IDay
+    {
+        private static readonly Regex _regex = new Regex(@"(\d+)-(\d+),(\d+)-(\d+)");
+
+        public string SolvePart1(IEnumerable<string> input)
+        {
+            return input
+                .Select(x => _regex.Match(x))
+                .Select(x => RangeHelpers.IsOneRangeFullyContained(x.Groups[1].Value, x.Groups[2].Value, x.Groups[3].Value, x.Groups[4].Value))
+                .Count(x => x).ToString();
+        }
+
+        public string SolvePart2(IEnumerable<string> input)
+        {
+            return input
+                .Select(x => _regex.Match(x))
+                .Select(x => RangeHelpers.HasOverlap(x.Groups[1].Value, x.Groups[2].Value, x.Groups[3].Value, x.Groups[4].Value))
+                .Count(x => x).ToString();
+        }
+    }
+}
---
+++ Utilities/RangeHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class RangeHelpers
+    {
+        public static bool HasOverlap(string xstart, string xend, string ystart, string yend)
+        {
+            return HasOverlap(int.Parse(xstart), int.Parse(xend), int.Parse(ystart), int.Parse(yend));
+        }
+
+        public static bool HasOverlap(int xstart, int xend, int ystart, int yend)
+        {
+            return xstart <= yend && ystart <= xend;
+        }
+
+        public static bool IsOneRangeFullyContained(string xstart, string xend, string ystart, string yend)
+        {
+            return IsOneRangeFullyContained(int.Parse(xstart), int.Parse(xend), int.Parse(ystart), int.Parse(yend));
+        }
+
+        public static bool IsOneRangeFullyContained(int xstart, int xend, int ystart, int yend)
+        {
+            var minStart = Math.Min(xstart, ystart);
+            var maxStart = Math.Max(xend, yend);
+            return (xstart == minStart && xend == maxStart) || (ystart == minStart && yend == maxStart);
+        }
+    }
+}
