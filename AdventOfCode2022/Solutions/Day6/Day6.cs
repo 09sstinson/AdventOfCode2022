bd1ecd8cddb9e5a9af209e316fd1d4b3705b37ae
09sstinson <09sstinson@gmail.com>
2022-12-06 20:24:50 +0000

    Day6

---
+++ AdventOfCode2022/Solutions/Day6/Day6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace AdventOfCode2022.Solutions
+{
+    public class Day6 : IDay
+    {
+        public string SolvePart1(IEnumerable<string> input)
+        {
+            return GetPositionOfDistinctValues(input.First(), 4).ToString();
+        }
+
+        public string SolvePart2(IEnumerable<string> input)
+        {
+            return GetPositionOfDistinctValues(input.First(), 14).ToString();
+        }
+
+        private int GetPositionOfDistinctValues(string input, int numberOfDistinctValues)
+        {
+            var buffer = new FixedSizeQueue<char>(numberOfDistinctValues);
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                buffer.Enqueue(input[i]);
+
+                if (buffer.IsDistinct())
+                {
+                    return i + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Utilities/FixedSizeQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class FixedSizeQueue<T>
+    {
+        private readonly Queue<T> _queue = new Queue<T>();
+
+        private readonly int _size;
+
+        public FixedSizeQueue(int size){
+            _size = size;
+        }
+
+        public void Enqueue(T value)
+        {
+            _queue.Enqueue(value);
+
+            if(_queue.Count > _size)
+            {
+                _queue.Dequeue();
+            }
+        }
+
+        public bool IsDistinct()
+        {
+            return _queue.Distinct().Count() == _size;
+        }
+    }
+}
